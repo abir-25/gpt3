e866a64e15ab65f4276b4a4f77d1785bae9c1e32
abir-25 <abirbgc25@gmail.com>
2023-05-04 00:41:08 +0600

    Project Structure is ready

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import React from "react";
 import "./App.css";
+import {
+  Blog,
+  Header,
+  Footer,
+  Possibility,
+  Features,
+  WhatGPT3,
+} from "./containers";
+import { Article, Brand, Cta, Feature, Navbar } from "./components";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
